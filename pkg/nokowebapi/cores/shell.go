aefa288ded33c4238294f8a7d793afb4ed296b5a
john-shika <skulluglify@outlook.com>
2024-11-06 20:07:05 +0700

    [feat] update files

--- pkg/nokowebapi/cores/shell.go
+++ pkg/nokowebapi/cores/shell.go
@@ -5,7 +5,7 @@ import (
 	"os/exec"
 )
 
-func makeProcess(path string, args []string, envs []string, stdin io.Reader, stdout io.Writer, stderr io.Writer) (cmd *exec.Cmd, err error) {
+func MakeProcess(path string, args []string, envs []string, stdin io.Reader, stdout io.Writer, stderr io.Writer) (cmd *exec.Cmd, err error) {
 	if path, err = exec.LookPath(path); err != nil {
 		return nil, err
 	}
